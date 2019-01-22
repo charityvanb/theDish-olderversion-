6e32c8069ed822af9923e314041169e21809b059
charityvanb <charityvanb@gmail.com>
2019-01-22 10:03:17 -0700

    Made a README file.

---
+++ TheDish2/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>theDish</h1>
+        </div>
+    );
+};
+
+ReactNative.AppRegistry.registerComponent('albums', () => App);
---
+++ readme.md
@@ -0,0 +1,2 @@
+##theDish
+TheDish is an app that does machine learning to identify vintage china.
